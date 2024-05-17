21ff9ebfec2acdc5a834904345ea2a3367cec151
@coder shruti <www.meshruti@gmail.com>
2024-05-18 04:18:39 +0530

    array in ts

---
+++ build/array.js
@@ -0,0 +1,4 @@
+"use strict";
+const arr = [1, 2, 3, 4];
+const arrr = [1, 4, 5];
+const arr2 = ["shru", 24, "dev", 23, "aman", 12, "anmol", 14];
---
+++ src/array.ts
@@ -0,0 +1,6 @@
+const arr:number[]=[1,2,3,4];
+const arrr: [number,number,number] =[1,4,5];
+
+const arr2: Array<string | number> = ["shru", 24, "dev", 23, "aman", 12, "anmol", 14]
+
+
