ec71b267dd61485a4192adc7c75f2dd27a28fd12
@coder shruti <www.meshruti@gmail.com>
2024-05-18 03:36:03 +0530

    variable in ts

---
+++ build/variable.js
@@ -0,0 +1,5 @@
+"use strict";
+let greetings = "Hello Shruti This Side,";
+console.log(greetings);
+greetings.toLowerCase();
+console.log(greetings);
---
+++ src/variable.ts
@@ -0,0 +1,6 @@
+let greetings : string = "Hello Shruti This Side,"
+
+console.log(greetings);
+
+greetings.toLowerCase();
+console.log(greetings);
